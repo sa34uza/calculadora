d90e3713ed56487e4c1ccc48b091a448468f0ad5
Savio Ferreira de Souza <log.savio@gmail.com>
2021-01-19 04:28:06 -0300

    iniciio da logica

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,11 +1,27 @@
 class Memory {
+  static const operations = const ['%', '/', 'x', '-', '+', '='];
+
+  final _buffer = ([0.0, 0.0]);
+  int _bufferIndex = 0;
+  String operation;
   String _value = '0';
+  bool _wipeValue = false;
 
   void applyCommand(String command) {
     if (command == 'AC') {
       _allClear();
+    } else if (operations.contains(command)) {
+      _setOperation(command);
     } else
-      _value += command;
+      _addDigit(command);
+  }
+
+  _setOperation(String newOperations) {
+    _wipeValue = true;
+  }
+
+  _addDigit(String digit) {
+    _value += digit;
   }
 
   _allClear() {
